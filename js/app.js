d63bb6643d1094d1581409ce89cd9235ed9fbec1
gonzalezjennifer <62447915+gonzalezjennifer@users.noreply.github.com>
2024-08-06 09:30:00 -0600

    se agrego el endpoint para buscar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 console.log('@@@ Conectando... ')
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadData(city = null)
+})
+
+const loadData = async (city) => {
+    if (!city) {
+        city = 'oceanside'
+    }
+    const url = `https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=${city}&limit=5`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'c653860161mshbb7b2839132e7f3p1a578ajsnbf31f680a026',
+            'x-rapidapi-host': 'realty-in-us.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log('@@@@ result =>', result);
+    } catch (error) {
+        console.error(error);
+    }
+}
